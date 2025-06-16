db96b2033050c6a48cac9e5a6ce691c25afcf715
Elisey <eliseyblajen@gmail.com>
2025-06-16 13:05:50 +0500

    Update routes import

--- app/liteika_api/routes/__init__.py
+++ app/liteika_api/routes/__init__.py
@@ -1,11 +1,7 @@
 from fastapi import APIRouter
 
-from app.liteika_api.routes import search
-from app.liteika_api.routes import compare
 from app.liteika_api.routes import faq
 
 router = APIRouter()
      
-router.include_router(search.router)
-router.include_router(compare.router)
 router.include_router(faq.router)
